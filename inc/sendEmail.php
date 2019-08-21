3ffaa05d60ca23823369895fbe37e2f3196f863c
“prateektiwari7” <prateek.tiwari456@gmail.com>
2019-08-21 17:11:16 +0530

    ADD

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -60,7 +60,7 @@ if($_POST) {
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
 		
-		echo $response;
+		echo $response ;
 
 	} # end if - there was a validation error
 
